604a1b0a8d1a44707e16b559e0630cd229640416
JRP <jamesroypoulter@gmail.com>
2013-07-08 18:54:58 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thecooboo::Application.config.secret_token = '81432d56a9ba7f45fadfaae4ef2eb1e3e7c4a02d088dd49e2060248baaf6b94c6a8fbc7f31889d1d66e251861838618e424d009b6979fecd12ebb496989c308d'
